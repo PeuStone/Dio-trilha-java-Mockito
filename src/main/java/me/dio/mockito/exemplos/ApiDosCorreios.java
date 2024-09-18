531bb1c645bee99e4ab25095a375adc942146135
PeuStone <pedrohmotaaa@gmail.com>
2024-09-18 18:31:07 -0300

    Criação de classes para exemplos e primeiro teste realizado.

---
+++ src/main/java/me/dio/mockito/exemplos/ApiDosCorreios.java
@@ -0,0 +1,12 @@
+package me.dio.mockito.exemplos;
+
+/**
+ * Classe fictícia apenas para integração com a API dos Correios
+ */
+
+public class ApiDosCorreios {
+
+    public DadosLocalizacao buscarDadosComBaseNoCep(String cep) {
+        return null;
+    }
+}
---
+++ src/test/java/me/dio/mockito/exemplos/CadastrarPessoaTeste.java
@@ -0,0 +1,35 @@
+package me.dio.mockito.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class CadastrarPessoaTeste {
+
+    @Mock
+    private ApiDosCorreios apiDosCorreios;
+
+    @InjectMocks
+    private CadastrarPessoa cadastrarPessoa;
+
+    @Test
+    void validarDadosDeCadastro() {
+        DadosLocalizacao dadosLocalizacao = new DadosLocalizacao("MG", "Belo Horizonte", "Rua 3", "Apto", "Centro");
+        Mockito.when(apiDosCorreios.buscarDadosComBaseNoCep("7986767")).thenReturn(dadosLocalizacao);
+        Pessoa pessoa = cadastrarPessoa.cadastrarPessoa("Lucas", "74689765", LocalDate.now(), "7986767");
+
+        assertEquals("Lucas", pessoa.getNome());
+        assertEquals("74689765", pessoa.getDocumento());
+        assertEquals("MG", pessoa.getEndereco().getUf());
+        assertEquals("MG", pessoa.getEndereco().getUf());
+        assertEquals("Apto", pessoa.getEndereco().getComplemento());
+    }
+}
